d690b5eccae9a563b9adebdb4e99ca36f2282abe
Misty <gyc990326@gmail.com>
2022-05-07 10:42:25 +0800

    Better wait interval for iTunes Login

--- workflow_helper/itunes_auto_login.py
+++ workflow_helper/itunes_auto_login.py
@@ -32,7 +32,7 @@ cleanAllDialog()
 
 # Calm down a bit before main window operations
 app.wait_cpu_usage_lower()
-time.sleep(2)
+time.sleep(5)
 
 debugTopWin()
 
@@ -40,7 +40,7 @@ debugTopWin()
 app.iTunes.Button11.click_input()
 
 app.wait_cpu_usage_lower()
-time.sleep(2)
+time.sleep(4)
 
 # Start logging in by clicking toolbar menu "Account"
 print("Clicking Account menu...")
@@ -57,23 +57,44 @@ if '&S' in popup.menu().item(1).text():
     # not log in
     popup.menu().item(1).click_input()
     app.wait_cpu_usage_lower()
-    time.sleep(6)
+    time.sleep(8)
     debugTopWin()
+
+    for i in range(60):
+        dialog = app.top_window()
+        dialogWrap = dialog.wait('ready')
+        assert dialogWrap.friendly_class_name() == 'Dialog'
+        time.sleep(1.0)
+        try:
+            if dialogWrap.window_text() == 'iTunes' \
+                and dialog.Edit1.wait('ready').window_text() == 'Apple ID' \
+                and dialog.Edit2.wait('ready').window_text() == 'Password' \
+                and dialog.Button0.wait('exists').window_text() == '&Sign In':
+                break
+        except Exception as e:
+            pass
     
+    app.wait_cpu_usage_lower()
+
     print("Setting login dialog edit texts")
-    dialog = app.top_window()
-    assert dialog.friendly_class_name() == 'Dialog'
+
     appleid_Edit = dialog.Edit1
-    pass_Edit = dialog.Edit2
+    appleid_Edit.wait('ready')
+    appleid_Edit.click_input()
+    appleid_Edit.type_keys(ACCOUNT)
     appleid_Edit.set_edit_text(ACCOUNT)
-    time.sleep(1)
-    pass_Edit.type_keys('123')
-    time.sleep(1)
+    time.sleep(3)
+
+    pass_Edit = dialog.Edit2
+    pass_Edit.wait('ready')
+    pass_Edit.click_input()
+    pass_Edit.type_keys(PASSWORD)
     pass_Edit.set_edit_text(PASSWORD)
-    time.sleep(1)
+    time.sleep(3)
     
     print("Clicking login button!")
     loginButton = dialog.Button0
+    loginButton.wait('ready')
     loginButton.click()
 else:
     print("Already logged in!")
